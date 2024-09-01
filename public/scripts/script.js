83f9b24a2ee47b14c770e628c17aaaa332134537
CJ Hess <cj.hess510@gmail.com>
2024-09-01 00:39:55 -0500

    CSS Redesign + Dark Mode Implementation
    Complete redesign of CSS in an attempt to make readability better and open up more learning opportunities. Designed a taskbar instead of using one from Bootstrap. Implemented Dark Mode functionality on each page. Still working on how I feel about the overall Color Theme.

---
+++ public/scripts/script.js
@@ -0,0 +1,11 @@
+function toggleDarkMode() {
+    let isDark = document.body.classList.toggle('dark-mode');
+    localStorage.setItem('darkMode', isDark ? 'enabled' : 'disabled');
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (localStorage.getItem("darkMode") === 'enabled') {
+        document.body.classList.add('dark-mode');
+    }
+});
+
